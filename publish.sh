e4bdff41706b5932dfd445059a37a6e0190a7a96
Acidus <acidus99@users.noreply.github.com>
2023-01-09 16:30:10 -0500

    update publish

--- publish.sh
+++ publish.sh
@@ -1,18 +1,18 @@
 # create clean output
-rm -rf ~/tmp/apple-ii-output
-mkdir ~/tmp/apple-ii-output
-cp -R Posts/* ~/tmp/apple-ii-output/.
+rm -rf output
+mkdir output
+cp -R Posts/* output/.
 
 #convert MD to gemtext
-md2gemini --write --dir ~/tmp/apple-ii-output/ --frontmatter --img-tag='' --links images-only ~/tmp/apple-ii-output/*.md
-rm ~/tmp/apple-ii-output/*.md
+md2gemini --write --dir output/ --frontmatter --img-tag='' --links images-only output/*.md
+rm output/*.md
 
-#md2gemini annoyingly this adds CRLF
-dos2unix -q ~/tmp/apple-ii-output/*.gmi
+#md2gemini annoyingly adds CRLF, regardless of platform
+dos2unix -q output/*.gmi
 
 #fix escaped square brackets in link lines
-sed -i '' '/^=>/ s/\\\[/\[/' ~/tmp/apple-ii-output/*.gmi
-sed -i '' '/^=>/ s/\\\]/\]/' ~/tmp/apple-ii-output/*.gmi
+sed -i '' '/^=>/ s/\\\[/\[/' output/*.gmi
+sed -i '' '/^=>/ s/\\\]/\]/' output/*.gmi
 
 #generate index and add navigation links to the bottom of the posts
-dotnet run --project ../ContentIndexer/ ~/tmp/apple-ii-output/ "Apple II History"
+dotnet run --project ../ContentIndexer/ output/ "Apple II History"
