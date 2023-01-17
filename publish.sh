caa7f78a214f3860442a191c68a5b385a46ba047
Acidus <acidus99@users.noreply.github.com>
2023-01-17 07:59:04 -0500

    cleanup gallery

--- publish.sh
+++ publish.sh
@@ -24,3 +24,4 @@ mogrify -strip -quality 80 -resize 800x800\> output/images/*
 grep -E -h '^!\[' Posts/*.md | > output/gallery.md
 printf '# Image Gallery\n\n' > output/gallery.gmi
 perl -pe 's/!\[(.+)?\s\\\[\d+\\]\]\(([^\)]+)\)/=> $2 $1/' output/gallery.md >> output/gallery.gmi
+rm -f output/gallery.md
