a8069374b7cf86917752cbc39bcc2cc3e1e1d1f4
Acidus <acidus99@users.noreply.github.com>
2023-01-17 07:58:26 -0500

    generate image gallery in publish script

--- publish.sh
+++ publish.sh
@@ -3,26 +3,24 @@ rm -rf output
 mkdir output
 cp -R Posts/* output/.
 
-#convert MD to gemtext
+# convert MD to gemtext
 md2gemini --write --dir output/ --frontmatter --img-tag='' --links images-only output/*.md
 rm output/*.md
 
-#md2gemini annoyingly adds CRLF, regardless of platform
+# md2gemini annoyingly adds CRLF, regardless of platform
 dos2unix -q output/*.gmi
 
-#fix escaped square brackets in link lines
+# fix escaped square brackets in link lines
 sed -i '' '/^=>/ s/\\\[/\[/' output/*.gmi
 sed -i '' '/^=>/ s/\\\]/\]/' output/*.gmi
 
-#generate index and add navigation links to the bottom of the posts
+# generate index and add navigation links to the bottom of the posts
 dotnet run --project ../ContentIndexer/ output/ "Apple II History"
 
-#optimzie images for gemini
+# optimzie images for gemini
 mogrify -strip -quality 80 -resize 800x800\> output/images/*
 
-#make image gallery
-#grep -E -h '^!\[' Posts/*.md > output/gallery.md
-#md2gemini --write --dir output/ --frontmatter --img-tag='' --links images-only output/gallery.md
-#dos2unix -q output/gallery.gmi
-#remove the footnote ref entirely
-#sed -i '' '/^=>/ s/\\\[\d+\\\]//' output/gallery.gmi
+# make image gallery
+grep -E -h '^!\[' Posts/*.md | > output/gallery.md
+printf '# Image Gallery\n\n' > output/gallery.gmi
+perl -pe 's/!\[(.+)?\s\\\[\d+\\]\]\(([^\)]+)\)/=> $2 $1/' output/gallery.md >> output/gallery.gmi
